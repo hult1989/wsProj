9aaba96b00fc89568fe5441aa6cce405f086265f
hult <kindth@qq.com>
2015-10-17 10:29:56 +0800

    remove cached sqlPool file

--- sqlPool.py
+++
@@ -1,4 +0,0 @@
-from twisted.enterprise import adbapi
-
-dbpool = adbapi.ConnectionPool("MySQLdb", db="wsdb", user='tanghao', passwd='123456')
-#dbpool = adbapi.Connection("MySQLdb", db="wsdb", user='tanghao', passwd='123456', unix_socket='/tmp/mysql.sock')
