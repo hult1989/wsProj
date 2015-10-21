218c3a058de80471a0a3703b5fce160be43198a6
hult <kindth@qq.com>
2015-10-21 16:31:24 +0800

    rename log file

--- init.py
+++ init.py
@@ -7,7 +7,7 @@ from twisted.python import log
 
 application = service.Application('WalkingStickServer')
 sc = service.IServiceCollection(application)
-log.startLogging(open('./init.log', 'w'))
+log.startLogging(open('./server.log', 'w'))
 appService = internet.TCPServer(8082, Site(mainPage))
 tcpService = internet.TCPServer(8081, WsServerFactory(dbpool))
 appService.setServiceParent(sc)
