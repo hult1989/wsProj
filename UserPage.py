bd3ed16ec681be3549c6bc6d2298259ab01d5824
hult <kindth@qq.com>
2015-10-21 15:41:56 +0800

    remove all definition of *Page class from appServer file to *Page.py file

---
+++ UserPage.py
@@ -0,0 +1,123 @@
+from json import dumps, loads
+from twisted.python import log
+from twisted.internet import reactor
+from twisted.web.resource import Resource
+from twisted.web.server import Site, NOT_DONE_YET
+import cgi
+from appServerCommon import onError, resultValue
+from sqlhelper import *
+from sqlPool import dbpool
+
+
+class UserPage(Resource):
+    isLeaf = True
+
+    def checkUser(self, result, payload):
+        if len(result) != 0:
+            d = defer.Deferred()
+            d.callback('400')
+            return d
+        else:
+            return insertUserSql(dbpool, username=payload['username'], passwd=payload['password'])
+
+    def onResult(self, result, request):
+        if result in ['400', '401', '402', '403', '404']:
+            request.write(resultValue(result))
+        elif type(result) == tuple and len(result) != 0:
+            request.write(dumps({'result': '1', 'imei': result[0][1], 'name': result[0][2], 'simnum': result[0][3]}))
+        elif type(result) == tuple and len(result) == 0:
+            request.write(resultValue(404))
+        else:
+            request.write(resultValue(1))
+        request.finish()
+
+    
+    def onLogin(self, result, request, payload):
+        if len(result) == 0:
+            d = defer.Deferred()
+            d.callback('401')
+            return d
+        if payload['password'] != result[0][1]:
+            d = defer.Deferred()
+            d.callback('402')
+            return d
+        if request.args['action'] == ['login']:
+            return selectLoginInfoSql(dbpool, payload['username'])
+        if request.args['action'] == ['updatepassword']:
+            return UpdateUserPasswordSql(dbpool, username=payload['username'], newpassword=payload['newpassword'])
+
+    def onRegister(self, result, request):
+        if str(result) == '400':
+            request.write(resultValue(result))
+        else:
+            request.write(resultValue('1'))
+
+        request.finish()
+        
+    
+    def onChangeName(self, result, payload):
+        if len(result) == 0:
+            return '403'
+        else:
+            return updateStickNameSql(dbpool, username=payload['username'], imei=payload['imei'], name=payload['name'])
+
+
+    def onGetSticks(self, result, request):
+        if len(result) == 0:
+            request.write(resultValue(404))
+        else:
+            sticks = list()
+            for r in result:
+                stick = dict()
+                stick['simnum'] = str(r[2])
+                stick['name'] = str(r[1])
+                stick['imei'] = str(r[0])
+                sticks.append(stick)
+            request.write(dumps({'result': '1', 'sticks': sticks}))
+        request.finish()
+
+    def onUpload(self, result, request):
+        if result:
+            request.write(resultValue(1))
+        else:
+            request.write(resultValue(403))
+        request.finish()
+
+    def render_POST(self, request):
+        payload = eval(request.content.read())
+
+        if request.args['action'] == ['register']:
+            d = selectUserSql(dbpool, payload['username']).addCallback(self.checkUser, payload)
+            d.addCallback(self.onRegister, request)
+            d.addErrback(onError)
+            return NOT_DONE_YET
+
+        if request.args['action'] == ['login']:
+            d = selectUserSql(dbpool, payload['username']).addCallback(self.onLogin, request, payload).addCallback(self.onResult, request)
+            d.addErrback(onError)
+            return NOT_DONE_YET
+        if request.args['action'] == ['updatepassword']:
+            d = selectUserSql(dbpool, payload['username']).addCallback(self.onLogin, request, payload).addCallback(self.onResult, request)
+            d.addErrback(onError)
+            return NOT_DONE_YET
+
+        if request.args['action'] == ['setstickname']:
+            d = selectRelationByImeiSql(dbpool, payload['username'], payload['imei']).addCallback(self.onChangeName, payload)
+            d.addCallback(self.onResult, request)
+            d.addErrback(onError)
+            return NOT_DONE_YET
+
+        if request.args['action'] == ['getsticks']:
+            d = selectRelationSql(dbpool, payload['username'])
+            d.addCallback(self.onGetSticks, request)
+            d.addErrback(onError)
+            return NOT_DONE_YET
+
+        if request.args['action'] == ['uploadsticks']:
+            d = handleUploadSql(dbpool, payload)
+            d.addCallback(self.onUpload, request)
+            d.addErrback(onError)
+            return NOT_DONE_YET
+
+userPage = UserPage()
+
--- appServer.py
+++ appServer.py
@@ -14,106 +14,6 @@ def onError(failure):
     log.msg(failure)
 
 
-
-
-class UserPage(Resource):
-    isLeaf = True
-
-    def checkUser(self, result):
-        if len(result) != 0:
-            d = defer.Deferred()
-            d.callback('400')
-            return d
-        else:
-            return insertUserSql(dbpool, username=self.payload['username'], passwd=self.payload['password'])
-
-    def onResult(self, result):
-        if result in ['400', '401', '402', '403', '404']:
-            self.request.write(resultValue(result))
-        elif type(result) == tuple and len(result) != 0:
-            self.request.write(dumps({'result': '1', 'imei': result[0][1], 'name': result[0][2], 'simnum': result[0][3]}))
-        elif type(result) == tuple and len(result) == 0:
-            self.request.write(resultValue(404))
-        else:
-            self.request.write(resultValue(1))
-        self.request.finish()
-
-    def onLogin(self, result):
-        if len(result) == 0:
-            d = defer.Deferred()
-            d.callback('401')
-            return d
-        if self.payload['password'] != result[0][1]:
-            d = defer.Deferred()
-            d.callback('402')
-            return d
-        if self.request.args['action'] == ['login']:
-            return selectLoginInfoSql(dbpool, self.payload['username'])
-        if self.request.args['action'] == ['updatepassword']:
-            return UpdateUserPasswordSql(dbpool, username=self.payload['username'], newpassword=self.payload['newpassword'])
-
-    def onRegister(self, result):
-        if str(result) == '400':
-            self.request.write(resultValue(result))
-        else:
-            self.request.write(resultValue('1'))
-
-        self.request.finish()
-        
-    
-    def onChangeName(self, result):
-        if len(result) == 0:
-            return '403'
-        else:
-            return updateStickNameSql(dbpool, username=self.payload['username'], imei=self.payload['imei'], name=self.payload['name'])
-
-
-    def onGetSticks(self, result):
-        if len(result) == 0:
-            self.request.write(resultValue(404))
-        else:
-            sticks = list()
-            for r in result:
-                stick = dict()
-                stick['simnum'] = str(r[2])
-                stick['name'] = str(r[1])
-                stick['imei'] = str(r[0])
-                sticks.append(stick)
-            self.request.write(dumps({'result': '1', 'sticks': sticks}))
-        self.request.finish()
-
-    def onUpload(self, result):
-        if result:
-            self.request.write(resultValue(1))
-        else:
-            self.request.write(resultValue(403))
-        self.request.finish()
-
-    def render_POST(self, request):
-        self.request = request
-        payload = eval(request.content.read())
-        self.payload = payload
-        log.msg(payload)
-        if request.args['action'] == ['register']:
-            selectUserSql(dbpool, payload['username']).addCallback(self.checkUser).addCallbacks(self.onRegister, onError)
-            return NOT_DONE_YET
-        if request.args['action'] == ['login']:
-            selectUserSql(dbpool, payload['username']).addCallback(self.onLogin).addCallbacks(self.onResult, onError)
-            return NOT_DONE_YET
-        if request.args['action'] == ['updatepassword']:
-            selectUserSql(dbpool, payload['username']).addCallback(self.onLogin).addCallbacks(self.onResult, onError)
-            return NOT_DONE_YET
-        if request.args['action'] == ['setstickname']:
-            selectRelationByImeiSql(dbpool, payload['username'], payload['imei']).addCallback(self.onChangeName).addCallbacks(self.onResult, onError)
-            return NOT_DONE_YET
-        if request.args['action'] == ['getsticks']:
-            selectRelationSql(dbpool, payload['username']).addCallbacks(self.onGetSticks, onError)
-            return NOT_DONE_YET
-        if request.args['action'] == ['uploadsticks']:
-            handleUploadSql(dbpool, payload).addCallbacks(self.onUpload, onError)
-            return NOT_DONE_YET
-
-
 class LocationPage(Resource):
     isLeaf = True
 
@@ -231,18 +131,20 @@ class WsinfoPage(Resource):
         log.msg(str(error))
 
 
-
 from sqlPool import dbpool
+from GpsPage import gpsPage
+from StickPage import stickPage
+from SosPage import sosPage
+from UserPage import userPage
+
 mainPage = Resource()
 apiPage = Resource()
 mainPage.putChild('api', apiPage)
-from GpsPage import gpsPage
+
 apiPage.putChild('gps', gpsPage)
-from StickPage import stickPage
 apiPage.putChild('stick', stickPage)
-from SosPage import sosPage
 apiPage.putChild('sos', sosPage)
-apiPage.putChild('user', UserPage())
+apiPage.putChild('user', userPage)
 
 
 mainPage.putChild('location', LocationPage())
